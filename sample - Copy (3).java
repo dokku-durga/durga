a8e9816e2cb1538ebd6fd4cdd6146e52c9bb7015
dokku-durga <kanakadurgadokku0@gmail.com>
2025-01-09 13:51:55 +0530

    Add files via upload
    hi team lookgood

---
+++ sample - Copy (3).java
@@ -0,0 +1,12 @@
+/ FileName : "GFG.java".
+
+public class GFG {
+  
+    // Program begins with a call to main() method
+    public static void main(String args[])
+    {
+      
+          // Prints "Hello World" 
+        System.out.println("Hello World");
+    }
+}
